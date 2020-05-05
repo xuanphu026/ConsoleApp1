391ca662862ab6353844ae998b1a76212d3a4493
x_phu <ngphu26@gmail.com>
2020-05-05 15:41:29 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int s = 0;
+            for (int i = 0; i <= a; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    s = s + i;
+                }
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+
+            //int num, sum = 0, r;
+            //Console.WriteLine("NHAP VAO MOT SO : ");
+            //int.Parse(Console.ReadLine());
+            //while (num != 0)
+            //{
+            //    r = num % 10;
+            //    num = num / 10;
+            //    sum = sum + r;
+            //}
+            //Console.WriteLine("TONG CAC CHU SO CUA NO LA : " + sum);
+            //Console.ReadLine();
+
+        }
+    }
+}
